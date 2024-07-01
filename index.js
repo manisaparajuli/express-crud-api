aadd908c00c5ce048e7cf5c06f20087845fbd0f0
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-01 14:29:10 +1000

    update url

--- index.js
+++ index.js
@@ -27,6 +27,43 @@ app.post('/api/products', async(req, res) => {
   }
 });
 
+app.get('/api/products', async(req, res) => {
+  try{
+    const products = await Product.find({});
+    res.status(200).json(products);
+  }catch(err){
+    res.status(500).json({message: err.message})
+  }
+})
+
+app.get('/api/product/:id', async(req, res) => {
+  try{
+    const {id}  = req.params ;
+    const product = await Product.findById(id);
+    res.status(200).json(product)
+  }catch(err){
+    res.status(500).json({message: err.message})
+  }
+})
+
+//update a product
+app.put('/api/product/:id', async(req, res) => {
+
+  try{
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if(!product) throw new Error(`Cannot find any product related to ${id} `)
+    
+    const updatedProduct = await Product.findById(id)
+    res.status(200).json(updatedProduct);
+  }
+  catch(err)
+  {
+    res.status(500).json({message: err.message})
+  }
+
+})
+
 
 mongoose.connect('mongodb+srv://manisaparajuli66:manisaparajuli66@backenddb.dz24xdo.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
   .then(() => {
