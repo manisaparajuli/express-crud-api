deab700ec5c71e757a16c83eb139d07a5bf9635f
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-01 12:26:22 +1000

    git ignore added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("this is index page")
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log(`server is running on prot 3000`)
+})
+
+app.get('/', (req, res) => {
+  res.send("Hello from node api")
+})
