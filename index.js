be4b77c59ab4c01c2b6a787bb2e533d36d7a0f20
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-01 13:05:38 +1000

    add

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model')
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`server is running on prot 3000`)
-})
+app.use(express.json())
+
+
+
+// app.listen(3000, () => {
+//   console.log(`server is running on prot 3000`)
+// })
+
 
 app.get('/', (req, res) => {
-  res.send("Hello from node api")
+  res.send("Hello from node api  ")
 })
+
+
+app.post('/api/products', async(req, res) => {
+  try{
+    const product = await Product.create(req.body);
+    res.status(200).json(product)
+  }catch(err){
+    res.status(500).json({message: err.message})
+  }
+});
+
+
+mongoose.connect('mongodb+srv://manisaparajuli66:manisaparajuli66@backenddb.dz24xdo.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
+  .then(() => {
+    console.log('connected to db')
+    app.listen(3000, () => {
+      console.log(`server is running on prot 3000`)
+    })
+  })
+  .catch(() => {
+    console.log('connection failed')
+  })
+
+
