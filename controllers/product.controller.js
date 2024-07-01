8615ae17ab7d6577741c48c15893d8a6d92de5c0
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-01 17:26:56 +1000

    CRUD API

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require('../models/product.model')
 
 const getProducts = async (req, res) => {
   try{
-    const product = await Product.create(req.body);
+    const product = await Product.find({});
     res.status(200).json(product)
   }catch(err){
     res.status(500).json({message: err.message})
